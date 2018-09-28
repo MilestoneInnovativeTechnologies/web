0a1b517707ae0f0baca8f0b8fc1fe74f87b964c2
Shareena KE <shareenaanoop@gmail.com>
2018-09-28 17:07:53 +0530

    git commit -m "Registration form"

--- laravel/resources/views/dbStore/DBBackupRegistration.blade.php
+++ laravel/resources/views/dbStore/DBBackupRegistration.blade.php
@@ -5,8 +5,6 @@
 
 
 
-
-
     <form method="post" enctype="multipart/form-data" >
         {{ csrf_field() }}
 
@@ -17,23 +15,36 @@
             <div class="panel-body">
 
                 <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-3 control-label" style="padding-left: 200px">Name</label>
+                    <label for="DBName" class="col-sm-4 control-label" style="padding-left: 200px">Database Name</label>
                     <div class="col-sm-5">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Name">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Database Name">
                     </div>
                 </div>
                 <br><br>
-                    <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-3 control-label" style="padding-left: 200px">Name</label>
+                 <div class="form-group">
+                    <label for="DBPassword" class="col-sm-4 control-label" style="padding-left: 200px">Database Password</label>
+                     <div class="col-sm-5">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Database Password">
+                    </div>
+                 </div> <br><br>
+                <div class="form-group">
+                    <label for="BackupType" class="col-sm-4 control-label" style="padding-left: 200px">Backup Types</label>
                     <div class="col-sm-5">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Name">
+                        <div class="dropdown">
+
+                                <select class="form-control"  >
+                                    <option>--Select--</option>
+                                    <option>  7 Days </option>
+                                    <option> 30 Days </option>
+                                    <option>1 Year </option>
+                                </select>
+
+                        </div>
                     </div>
                     </div>
-
-
-            </div>
+                </div>
             <div class="panel-footer clearfix">
-                <input type="submit" name="submit" value="Upload" class="btn btn-primary pull-right">
+                <input type="submit" name="Register" value="Register" class="btn btn-primary pull-right" >
             </div>
         </div>
     </form>
