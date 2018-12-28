45cf85b3cde0d7f4eae7f5224268ae17ed2f8294
Firose Hussain <github@firumon.com>
2018-12-28 17:43:58 +0530

    Public Article - Fiscal Year 2019 Adding

---
+++ laravel/resources/views/pa/articles/add_new_fiscal_year_eplus_2019.blade.php
@@ -0,0 +1,75 @@
+@extends("pa.articles.layout")
+@section('content')
+
+<div class="page_contents">
+<div class="page pa" style="background-color: #EBEBEB">
+<div class="container">
+<br><br><br><br><br><br><br><h1><b><u><center>FISCAL YEAR</center></u></b></h1>
+<p><center><b>(Helps to record the business data in year-wise and provides options to adjust duration of a fiscal year)</b></center></p>
+<h2><u><center>STEPS TO ADD NEW FISCAL YEAR ON SOFTWARE (2019)</center></u></h2>
+<h3 align="left"><b><u>Step 1:</u></b></h3>
+<p align="left"><b>From Tools menu, select Settings->Fiscal year</b></p>
+<image src="https://lh3.googleusercontent.com/3NgEztNOpy_cKtunVEnbxj-k8_gRM_hzVQiIlJN9G9FGp8KvqJWPmfHV4QQUh8WjN0w0C_lupZL6OMyNPSSdiku8PSBPVI-AfVeOCYQ81xRJc1CIXSZdRf3EjkwmnlJAjPIivOUKZ12NgAUCfTbcBip7YswXZhYxSvqJ6P-6U0xvwb30rs_gBbjeEiHYt44M5Mu_1KlYyKD89qdx9BUcMV9iXA1CSyWrsEQTTcmd2VhtcoV6gByZ9G7eNexxm0301Zgyu3MiXiTVN1fv25CIeUZPFHX2ddxYCJMgZoNu35hpo9MTqWMkU4GXvs8VrmSKuwk-NblV41tWKjGBAQ-wDmbfx6aO80g_HlLM6wQrZl3PlcgMblP1lr_6Hrkv5YYqi7Hucr4VeKIuHJJJPhPV8u727f6Veg6KLY32YhUEZ3bwtzX3GMRwvM1PiQc0f9uEuojKuSc2xtKlVYPfdoGzoaX5YhREy-OFPuFX1fVuGJlnHsSO5cHtCtrYGcmAEiZLmDqjpgu7w8E-Se1W59r6kpsGsNDiiIT11rkam-LOTasJoV_fERs42D27DY6kcfq98MbbAMxhwV--GN1I6OE8qPANa0V-ciiVD8RcC7nvYSjWjM0WGV1ih8rMJ4EMEI_52EKojFBSRymRyGmC=w1040-h686-no" alt="aa">
+<h3 align="left"><b><u>Step 2:</u></b></h3>
+<p align="left"><b>Select “New” option and enter the new fiscal year details as shown below.</b></p>
+<image src="https://lh3.googleusercontent.com/z2ef_waL0409zhbAIvAdT-cnVCcvnnGvkRfu6DxY01alwte4gfbEr_QoUHeZUqw2HjEmzTNIMSWU5021HYfPMXnjddRl_WV8jXpbv0QuRBQqKVk5_gM8Pd2LU7ToMhaMFNO4QJpbDR7sK85BujxfR3vtI_I7WdSbAew4WIjCnP-xbc5x5k-izq_19M0iRZTG-vi5Vhh33qC2YtP3y_leg-M8829XEwDXxNsaatT__0hWJkwJIzzzW5XuOKVhhabLaVbEafmec_W79Cgophv7bCNl4JVzwOgnQ_PfawUEjSVPwdjLjOZxFg_yR6k3iywxTJHyr6k6KMjBkYvMVJ_aLhjBZH7sbd6uCYXgYbSObesM4cQSQy-ObxAG6A0uhVAyCxNa0b_HWDeEIQT8NZuVuczLUykB-4AU2-T5I3a8ktXQ6fymjfgs6PGzlpWn14Hjy8E3VmCtCY2IPFIyTjKI814DPJBOeADal0p-DOO3GABs-2-paSo0p_C8565o67JtaxiAYNUaxVzm4WuKNTSJmBu4bygBzC6V299wqZu-6gZBhcqkfe8-f_GLGYskfb0Jh_JnMRp_YvYGy9JKHDiS_hmJScORmGIvBa5yMVpd0CXQEQdJieh-fwjmXj17vKqT4FpFj6mYUG4jnAhe=w845-h612-no" alt="bb">
+<h3 align="left"><b><u>Step 3:</u></b></h3>
+<p align="left"><b>Save and close the Application.</b></p>
+<h3 align="left"><b><u>Step 4:</u></b></h3>
+<p align="left"><b>Reopen the software.</b></p>
+<h3 align="left"><b><u>Step 5:</u></b></h3>
+<p align="left"><b>Now while opening any window a browse window will appear as shown below.</b></p>
+<p align="left"><b>You can select the fiscal year as your choice.</b></p>
+<image src="https://lh3.googleusercontent.com/0wGGJ59SAnXbtsPKaQ2flEfnnGR72_GNG2Z-xA3NypPEIgCfdzyQpA002bDN0xTMaLNTofyMzZqnIrP_8D5FOsC-czY0YOd6YjA5Au3eH3L-tJ6PZCtk40SJXlyFq1yEq1LU4DUC1oJHpHIyRqQlRkH97m2sszN5z2kRfVyX_sxr3K3lhx6DKnPG53vvQ_I2cTv4oOoVhoyNrqKJMtiJKPHHBcwrcUL-01NaMDM9heDf9QQMFMAezKV2xOSGdD0sGj68PeGV-XKDz8lloZ7Qg_BFlRQ8EBfmV6e2lJNziaqTooQJfCTRuflzZdwYhuYotn7r4I4XKBoe5qkZf9CfZwjjnUAQFMlfTubvuUTebs_UpEUiPpZ7q7p-vLd-P-zn3DyAEFV6JbdFCWqKNjQvL9XUwSIWLcQPaKqRQ1b4kZzlO5xplZSWjrHgYQEDXUV_hUAZJKrMPPf0cOMw3VpVJJK5iVuPSFMbbD3j8Io5m13IpvkZaD7lAeXLNUy9Kh2c-uCnVAvGg5p1MxTuK_2rEP8dW-A-DAhqyyclKw_hmwdKxGU6JkNMeYQSw8vBMzLUo3VGoAnW3Bf6NfI1vi_cqFQoAbp7-EmGCqELW8DCIBV17AWRfdJBv6doDmPkIPTIbQrLUDD1sHKngTLS=w893-h594-no" alt="cc">
+
+</div></div>
+
+
+
+
+
+
+		@include("home.section_contact_wo_map")
+	</div>
+	
+	<div id="loginModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content"><form method="post" action="{{ Route('login') }}" class="form-horizontal">{{ csrf_field() }}
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Login</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group clearfix">
+						<div class="col-xs-12">
+							<label class="control-label col-xs-4">Email Address:</label>
+							<div class="col col-xs-8">
+								<input type="text" name="email" value="" class="form-control">
+							</div>
+						</div>
+					</div>
+					<div class="form-group clearfix">
+						<div class="col-xs-12">
+							<label class="control-label col-xs-4">Password:</label>
+							<div class="col-xs-8">
+								<input type="password" name="password" value="" class="form-control">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+					<input type="submit" class="btn btn-primary" value="Login">
+				</div>
+			</form></div>
+		</div>
+	</div>
+@endsection
+@push('js')
+<script type="text/javascript">
+function login(){
+	$('#loginModal').modal('show');
+}
+</script>
+
+@endpush
