ce9f5d51c5a796da938c23d0198ed9b2ed9b429b
Firose Hussain <github@firumon.com>
2019-02-13 22:09:51 +0530

    API controls added

--- laravel/app/Http/Controllers/PDController.php
+++ laravel/app/Http/Controllers/PDController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CustomerRegistration;
+use App\Models\PD;
 use App\Models\PDTable;
 use App\Http\Requests\NewPD;
 
@@ -27,6 +29,46 @@ class PDController extends Controller
             foreach ($tbArray as $field) $pdtbl->$field = $request->$field[$name];
             $model->Tables()->save($pdtbl);
         }
+    }
+
+    public function interact($code){
+        $CR = $this->getCustomerReg($code);
+        $PD = $this->getCustomerPD($CR['customer'],$CR['seqno']);
+        $data = array_merge($CR,$PD);
+        return $data;
+    }
+
+    private function getCustomerReg($code){
+        list($key,$val) = KeyCodeController::Decode($code);
+        $where = array_combine($key,$val);
+        $cr = CustomerRegistration::with(['Product','Edition'])->where($where)->first();
+        return $cr ? $this->getCRArgs($cr) : null;
+    }
+    private function getCRArgs($cr){
+        return collect($cr)->only(['seqno','customer'])->merge(['product' => implode(" ",[$cr->Product->name,$cr->Edition->name,'Edition'])])->toArray();
+    }
+    private function getCustomerPD($customer,$seq){
+        $pd = PD::with(['Customer','Tables'])->where(compact('customer','seq'))->first();
+        return $pd ? $this->getPDArgs($pd) : null;
+    }
+    private function getPDArgs($pd){
+        return collect($pd)->only(['url_interact','code','id'])->merge(['customer' => $pd->Customer->name,'tables' => $pd->Tables->map(function($table){ return collect($table)->only(['table','last_created','last_updated']); })])->toArray();
+    }
+
+    public function intupd($code){
+        $data = $this->getIntUpdateArray($code);
+        $pd = PD::with('Tables')->find($data['id']); $md = ['updated','created'];
+        foreach($pd->Tables as $tbl){
+            foreach($md as $mode){
+                $key = join("_",[$tbl->table,$mode]); $fld = join("_",['last',$mode]);
+                $tbl->$fld = array_key_exists($key,$data) ? $data[$key] : $tbl->$fld;
+            }
+        }
+        $pd->push();
+    }
+    private function getIntUpdateArray($code){
+        list($key,$val) = KeyCodeController::Decode($code);
+        return array_combine($key,$val);
     }
 
 }
