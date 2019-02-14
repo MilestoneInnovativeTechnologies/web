0ac4d11b5d1fdf94fb1212cc70b8434b0024c384
Firose Hussain <github@firumon.com>
2019-02-14 12:38:41 +0530

    Added and Optimized API routes for PD

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -368,7 +368,8 @@ Route::prefix("v1")->group(function(){
 	
 });
 
-Route::group(["middleware"	=>	"wapi:*"],function(){
-    Route::get('pd/interact/{code}','PDController@interact');
-    Route::get('pd/interact/update/{code}','PDController@intupd');
+Route::group(["middleware"	=>	"wapi:*", "prefix" => "pd"],function(){
+    Route::get('/','PDController@api');
+    Route::get('interact/{code}','PDController@interact');
+    Route::get('interact/update/{code}','PDController@intupd');
 });
