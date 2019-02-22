aad8346c70dcc452009f6571ded023085cd95661
Firose Hussain <github@firumon.com>
2019-02-22 17:06:16 +0530

    New rule added to service provider

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        \Illuminate\Support\Facades\Validator::extend('AMCActiveCustomer',"\\App\\Rules\\AMCActiveCustomer@passes");
 //
 //			\DB::listen(function($Q){
 //				\Log::info('Query',[$Q->sql,$Q->bindings,$Q->time]);
