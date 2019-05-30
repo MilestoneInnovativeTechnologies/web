1551f0d7d3f5224fddb9209e07b5169cc712f8c6
Firose Hussain <github@firumon.com>
2019-05-30 10:32:29 +0530

    Reference field added

--- laravel/app/Http/Controllers/SmartSaleAssetController.php
+++ laravel/app/Http/Controllers/SmartSaleAssetController.php
@@ -19,10 +19,10 @@ class SmartSaleAssetController extends Controller
         'sales_order_items' => ['so,product,rate,quantity','USER','1','10'],
         'settings' => ['name,description,value','APP','24',''],
         'stock_transfer' => ['out,in,verified_by,verified_at','APPUSER','3',''],
-        'store_product_transactions' => ['store,product,direction,quantity,user,nature,date,type','USER','1','5'],
+        'store_product_transactions' => ['store,product,direction,quantity,user,nature,date,type,_ref','USER','1','5'],
         'stores' => ['name','APP','12',''],
-        'transaction_details' => ['transaction,spt,price,tax,discount,total','USER','1','5'],
-        'transactions' => ['user,docno,date,customer,fycode,fncode','USER','1','5'],
+        'transaction_details' => ['transaction,spt,price,tax,discount,total,_ref','USER','1','5'],
+        'transactions' => ['user,docno,date,customer,fycode,fncode,_ref','USER','1','5'],
         'user_settings' => ['user,setting,value','APPUSER','12',''],
         'user_store_area' => ['user,store,area','APPUSER','3',''],
         'users' => ['name','APP','6','']
