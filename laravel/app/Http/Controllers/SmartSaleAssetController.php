0b5b0e2dd1992ac4c366f92f4f220b2c4a75789b
Firose Hussain <github@firumon.com>
2019-06-29 15:39:33 +0530

    Transaction details, price -> amount

--- laravel/app/Http/Controllers/SmartSaleAssetController.php
+++ laravel/app/Http/Controllers/SmartSaleAssetController.php
@@ -22,7 +22,7 @@ class SmartSaleAssetController extends Controller
         'stores' => ['name,cocode,co_abr,brcode,br_abr','APP','12',''],
         'store_product_transactions' => ['store,product,direction,quantity,user,nature,date,type,_ref','USER','1','5'],
         'transactions' => ['user,docno,date,customer,fycode,fncode,payment_type,_ref','USER','1','5'],
-        'transaction_details' => ['transaction,spt,price,tax,discount,total','USER','1','5'],
+        'transaction_details' => ['transaction,spt,amount,tax,discount,total','USER','1','5'],
         'stock_transfer' => ['out,in,verified_by,verified_at','USER','3','1080'],
         'user_settings' => ['user,setting,value','APPUSER','12',''],
         'user_store_area' => ['user,store,area','APPUSER','3',''],
