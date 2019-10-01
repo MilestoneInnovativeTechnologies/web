d814f8ba4f56450fc0ea50d2bb6ed79fd9028a35
Firose <firumon@github.com>
2019-10-01 10:45:48 +0530

    Added menu and removed few tables

--- laravel/app/Http/Controllers/SmartSaleAssetController.php
+++ laravel/app/Http/Controllers/SmartSaleAssetController.php
@@ -6,8 +6,7 @@ class SmartSaleAssetController extends Controller
 {
     private $fields = [
         'setup' => ['name,value','APP','download'],
-        'product_transaction_natures' => ['name','APP','download'],
-        'product_transaction_types' => ['name','APP','download'],
+        'menu' => ['fncode,category,category_display,order,icon,name_home,name_drawer,status','APP','download'],
         'areas' => ['name','APP','download'],
         'stores' => ['name,cocode,co_abr,brcode,br_abr,currency','APP','download'],
         'users' => ['name,code,email,phone,address,outstanding,overdue','APP','both'],
@@ -16,23 +15,34 @@ class SmartSaleAssetController extends Controller
         'settings' => ['name,description,value','APP','download'],
         'user_settings' => ['user,setting,value','APPUSER','both'],
         'fiscalyearmaster' => ['code,name,abr,start_date,end_date','APP','download'],
-        'functiondetails' => ['code,format,digit_length,tax,taxselection,list,ratewithtax,discount01,discount02,discount02base,discount03,discountmode,discount','APP','download'],
+        'functiondetails' => ['code,format,digit_length,pricelist,tax,taxselection,list,ratewithtax,discount01,discount02,discount02base,discount03,discountmode,discount','APP','download'],
         'product_group_master' => ['name,code,list','APP','download'],
         'products' => ['name,uom,narration,taxcode01,taxfactor01,subtaxfactor01,taxcode02,taxfactor02,subtaxfactor02','APP','download'],
         'product_groups' => ['product,g01,g02,g03,g04,g05,g06,g07,g08,g09,g10','APP','download'],
         'pricelist_header' => ['name','APP','download'],
         'pricelist' => ['pricelist,product,price','APP','download'],
         'sales_order' => ['docno,date,user,customer,fycode,fncode,payment_type,progress,_ref','USER','both'],
-        'sales_order_items' => ['so,product,rate,quantity,taxrule,tax,discount,total,_ref','USER','both'],
-        'store_product_transactions' => ['store,product,direction,quantity,user,nature,date,type,_ref','USER','both'],
-        'transactions' => ['user,docno,date,customer,fycode,fncode,payment_type,_ref','USER','both'],
-        'transaction_details' => ['transaction,spt,amount,taxrule,tax,discount,total','USER','both'],
-        'sales_order_sales' => ['so,product,quantity,transaction,sale_quantity','USER','both'],
+        'sales_order_items' => ['so,product,rate,quantity,taxrule,tax,discount01,discount02,total,_ref','USER','both'],
+        'transactions' => ['user,docno,date,customer,store,fycode,fncode,payment_type,_ref','USER','both'],
+        'transaction_details' => ['transaction,product,quantity,direction,store,taxrule,tax,discount01,discount02,soi,total','USER','both'],
         'receipts' => ['docno,fycode,fncode,mode,customer,date,user,amount,bank,cheque,cheque_date,_ref,status','USER','both'],
         'stock_transfer' => ['out,in,verified_by,verified_at','USER','both'],
         'fn_reserves' => ['fncode,user,store,start_num,end_num,quantity,current,progress,status','USER','both'],
     ];
+    private $menu = [
+        //'NAME' => ['ComponentName','props<coma separated>','FNCodes<coma separated>']
+        'PURCHASE' => ['','fncode,store,fycode','PUR1,PUR2,PUR3,PUR4,PUR5','Inactive'],
+        'PURCHASE RETURN' => ['','fncode,store,fycode','PR1,PR2,PR3','Inactive'],
+        'PURCHASE ORDER' => ['','fncode,store,fycode','PO1','Inactive'],
+        'PAYMENT' => ['','fncode,store,fycode','CP1,BP1,BP2','Inactive'],
+        'SALES' => ['SalesIndex','fncode,store,fycode','SL1,SL2,SL3,SL4,SL5','Active'],
+        'SALES RETURN' => ['SalesReturnIndex','fncode,store,fycode','SR1,SR2','Active'],
+        'SALES ORDER' => ['SalesOrderIndex','fncode,store,fycode','SO1,SO2','Active'],
+        'RECEIPT' => ['CashReceiptIndex','fncode,store,fycode','CR1,BR1,BR2','Active'],
+        'MATERIAL TRANSFER' => ['MaterialTransferIndex','fncode,store,fycode','MT1,MT2','Active'],
+
+    ];
     public function index(){
-        return $this->fields;
+        return ['db' => $this->fields, 'menu' => $this->menu];
     }
 }
