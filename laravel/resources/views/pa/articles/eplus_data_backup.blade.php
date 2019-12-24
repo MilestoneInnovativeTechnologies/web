331595901c02d60a463951404f0498e1db16981b
Firose <firumon@github.com>
2019-12-24 16:33:20 +0530

    New public article for ePlus data backup created

---
+++ laravel/resources/views/pa/articles/eplus_data_backup.blade.php
@@ -0,0 +1,108 @@
+@extends("pa.articles.layout")
+@section('content')
+
+<div class="page_contents">
+	<div class="page pa" style="background-color: #EBEBEB">
+		<div class="container">
+
+
+			<br><br><br><br><br><br><br>
+			<h1><b><u><center>Backup</center></u></b></h1>
+			<p style="text-align: center; font-weight: bold;">Backups can be used to recover data after it lose from data deletion or to recover data from an earlier time. ePlus provide backup facility. </p>
+			<h2><u><center>How to take ePlus Backup?</center></u></h2>
+			<h3 align="left"><b><u>Step 1:</u></b></h3>
+			<p align="left"><b>Go To Tools->Backup ePlus</b></p>
+			<img src="https://lh3.googleusercontent.com/Jo-H51JpKw7iUGdrDv27ekTKxWqjrQcdt2-YZJWQtGBLjLvg9gpxUy7ACLeesx3W0KUjyTvLkn26nxfSiUrwwBre16ZesoXseNw-GV0eYmnmuxvpQdRHXkORa9MR9huloicvU4Zd=w277-h403-no"
+				 alt="ePlus Backup">
+			<h3 align="left"><b><u>Step 2:</u></b></h3>
+			<p align="left"><b>Below window will appear. Please select Backup button in order to take ePlus backup. </b>
+			</p>
+			<img src="https://lh3.googleusercontent.com/Xj1m2TJdNnDiaAOw9xP8FqrA6tAALAOX3RxTpnagJpsnFuWX9yZd-EA_AOQUqF7Fy7kPVtNjlATAdVKqJCTbAKgIgea-YE3g1gbQO7MBlny9tpKA5CQ-DAbgyW8t3DdvAVY_klFj=w656-h280-no"
+				 alt="ePlus backup window">
+			<h3 align="left"><b><u>Step 3:</u></b></h3>
+			<p align="left"><b>Select the location where you want to save the backup. As shown in the picture. And click
+					on Save button.</b></p>
+			<img src="https://lh3.googleusercontent.com/z9YzAEdCnMjlttgLafhohPdKKmbr8ofeFiqN5rge3_GfYUW0XiBWmJv99gOy1xJ8sLR7IXn6HweHQVvDNsK1FQhSG3P85vgXCcXp5v2zWZk3mhLFCictO5WNPgNEaLZAQf5BzWxg=w689-h493-no"
+				 alt="ePlus backup browse window">
+			<br><br><br><br><br><br><br>
+			<h1 style="text-align: center; font-weight: bold; text-decoration: underline;">How to upload backup to our site?</h1>
+			<p style="font-weight: bold; text-align: center">Our site will store your data when your system is crash.</p>
+			<h3 align="left"><b><u>Step 1:</u></b></h3>
+			<p align="left"><b>Once you took backup, make it .rar file in order to make the smaller file size.
+					Right click the mouse on the backup and click Add to .rar file as shown below.
+				</b></p>
+			<img src="https://lh3.googleusercontent.com/k2Jr_CLBAJsOsAvNVB40Fj0CQ9xfCGxabXQES02U-DaQU-_-mAwoInJrq1amUGWJDWwk8z8-LzpG-kPMIQBRrk2t-0LXjUjOmyoFpCEO4r1Mqc-QkjeUeil51VG1iLPcD8OPnslhKNnJ1pdhyQI_Hocc35IiPYkv_U5WPjPJD7nGz1xwCGo0Sb9EaJfqbOexi2fzjuyv8PyexA1edjZsFCWc6XU46mk8-_47PEom6NHoIPTWW8nAfzgEc_vsQxd1UKZkAI21Ssjasth6yCobFZrzsvRAKoqma3kQniQBvoycAyol_phscxFWTzLnlMOLf2Ii4ZBgC3o2NbgesHmJogkv6mmN2KoOfaDdihriE7mdLsaJHcFWx4H9Rys_34vlGlI1IaVRsL31RDRjTmFE4BOK4ZPXIhMp74cx8NquBY0YQ2RSSv0-791dP6otODlaAjKRGAIQq5bTafF1Wa97_SFvAiGcxQ8Yt6C0ThjWFm6rzvwuI5qVkbISVOg1wjVgTbAaL7GdznNoApxahc6BdodJxQRGHCSVvQikZOBOSIK7m_lVwMJRa8vIllCtfxwUXhrQltsMAVvcCFMl_J_Ro1AoW2J7EKV-3IpwtisJZ3rLo27FBNiMtWYzAp0jRoLc2ypVU3oCIKbSwzanHvEZ1dpNF3cwdbtO0zMC4os9G3babDw5_xzfU4jHp-yVQhrSm9ikT3ZMnfH8hZumTFwEC3orUCn2vmYmNtYeqgnQ=w589-h553-no"
+				 alt="dd">
+			<h3 align="left"><b><u>Step 2:</u></b></h3>
+			<p align="left"><b>Open our site <a href="http://milestoneit.net" target="_blank">www.milestoneit.net</a>, then click on Login, a window will appear for you to
+					enter the Email Address and Password ( the mail id u given at the time of purchase of ePlus ) and
+					click on Login button.
+				</b></p>
+			<img src="https://lh3.googleusercontent.com/rD8yXorIFyXU-1Ix-wG0bf1Th6gTW6I4HUzQKL5WF9a2agg3swQh_c-mDVgCPfkqwrOIE7yleVgzy_TWl9GWFlBihxjRrV8fyy38axjR0Rk0ONVA_WNefjUFcQl7-Xe83K846HAG=w652-h326-no"
+				 alt="ee">
+			<h3 align="left"><b><u>Step 3:</u></b></h3>
+			<p align="left"><b>After login, a window will appear which contain your ePlus details like ePlus version,
+					latest updation of ePlus, backups, print objects(invoice), active tickets etc. as shown below.</b>
+			</p>
+			<img src="https://lh3.googleusercontent.com/ivMrdYHooRI-jFLoAmSC212L1bOTgLCOf_igAFTgSNa29J6BS1vKTL1X2YbTnkBhEsrMg8u7OF_zpmAX69-6wptwFBN_duBpd6YnkKAYCHqbwJJQI8s4tJEUlQ_wiO56rEuLktqG=w1054-h657-no"
+				 alt="ff">
+			<h3 align="left"><b><u>Step 4:</u></b></h3>
+			<p align="left"><b>Click on Database Backups tab, a window will show your latest backup stored in our site.
+					To upload new backup, click on upload backup button as shown below.</b></p>
+			<img src="https://lh3.googleusercontent.com/8alJIsUIHYiFwGLO5l5h8hMzOcwJdPwu1BlOUIdl2zDx1QpWM1_I-S9uV4pTmId7ClohNCEJ5CIT3IyBBzgt85lwSJeNBD0hauLfMaPZMgchSq3MyJ5vsuvdfrJ8Jp3JcO1qt-4Q=w1229-h388-no"
+				 alt="gg">
+			<h3 align="left"><b><u>Step 5:</u></b></h3>
+			<p align="left"><b>A window will appear for uploading backup stored in your PC. Select the file by clicking
+					on Choose file.</b></p>
+			<img src="https://lh3.googleusercontent.com/p-hBUGDUJPuLLNK-DoYM2WcPIvVY12A68ooO51F5W8bkfKWnpwNvaITdr7yMKM9gED5iVLRtDWgcdzyM84nKviMCBXLIUk1UeYtmmnM_wO5KN4wIlQEDzbeY2tQw1_ScWF0Nw3-4=w976-h617-no"
+				 alt="hh">
+			<h3 align="left"><b><u>Step 6:</u></b></h3>
+			<p align="left"><b>After choosing the file, click on Upload button.</b></p>
+			<img src="https://lh3.googleusercontent.com/VqHIy54uyJONO4mEeulonTUjL0sjzfeSCMYKsLdHLjK88KO1Pz_IWLG5MIZkxc-Ypxs4CBqKY41v7w3ww3kUS4G60zWAqK0MqeqMe2XWGeImdJntNXL2WTBF5dHrH9Rl-QIEUoBf=w1169-h404-no"
+				 alt="ii">
+
+
+		</div>
+	</div>@include("home.section_contact_wo_map")</div>
+	
+	<div id="loginModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content"><form method="post" action="{{ Route('login') }}" class="form-horizontal">{{ csrf_field() }}
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Login</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group clearfix">
+						<div class="col-xs-12">
+							<label class="control-label col-xs-4">Email Address:</label>
+							<div class="col col-xs-8">
+								<input type="text" name="email" value="" class="form-control">
+							</div>
+						</div>
+					</div>
+					<div class="form-group clearfix">
+						<div class="col-xs-12">
+							<label class="control-label col-xs-4">Password:</label>
+							<div class="col-xs-8">
+								<input type="password" name="password" value="" class="form-control">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+					<input type="submit" class="btn btn-primary" value="Login">
+				</div>
+			</form></div>
+		</div>
+	</div>
+@endsection
+@push('js')
+<script type="text/javascript">
+function login(){
+	$('#loginModal').modal('show');
+}
+</script>
+
+@endpush
