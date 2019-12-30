acf6f8e6f16c2d01011a43400e7435bd580922c1
Firose <firumon@github.com>
2019-12-30 10:47:53 +0530

    Public article for adding fiscal year for year 2020 created

---
+++ laravel/resources/views/pa/articles/fiscal_year_2020_jan.blade.php
@@ -0,0 +1,86 @@
+@extends("pa.articles.layout")
+@section('content')
+
+<div class="page_contents">
+	<div class="page pa" style="background-color: #EBEBEB">
+		<div class="container">
+
+
+			<br><br><br><br><br><br><br>
+			<h1><b><u>
+						<center>Fiscal Year</center>
+					</u></b></h1>
+			<p style="text-align: center; font-weight: bold">
+			A fiscal year is a one-year period that companies and governments use for financial reporting and
+					budgeting. A fiscal year is most commonly used for accounting purposes to prepare financial
+					statements.
+			</p>
+
+			<h2><u>
+					<center>How to add new fiscal year on software (2020)</center>
+				</u></h2>
+			<h3 align="left"><b><u>Step 1:</u></b></h3>
+			<h2 align="left"> >> Open the ePlus software.</h2>
+			<h2 align="left"> >> In Tools menu select the option fiscal year.</h2>
+			<p align="left"><b>Go to Tools >Settings > Fiscal year</b></p>
+			<img src="https://lh3.googleusercontent.com/_anqldPfqC-gJg1y3rPDpRUlMgsStHNRjCteDZtC093fIIvMeKyERm2nfYqfXO4ng-cBq12l8Yeu7mmPY1Oo5zmb3kk5dbj1CVs5J-pLrrrJ36bkHwPA8yv7F50IfsBIiVxKZ96Pz4FCd1GtJyy_hLkGVtI8hf3refq_QIkMX88Wcz0GuSZJOjqSgixkAhjsJspIf27JWduQmYIp9lu_q3ALWqSOlEvxhGfTANkQwJ7OBHLPA8GQIjd4JmxCePlb2inShrCc4hZEPp0VDpEgiN0htRp5b669_japhDh-TM8zIJ-tZmJx6UiDRlBexYF1eCUL7zXCWTnqW6iIXnYdRAxWZan-JF-7ixwa3hwyY-j9F-hVjwv-X5xd8qZoRU1FeHK0HIZtaUgFHyRoAnRuSwhRvDa2K-sT_3PdiOTRR_jdkGmlecNlXFnquhDvQVQdmNxwi7l2ryK8ilvfooG3iIzCf1TAhR8aPLgEbtDCgK2xP4y4Ckoy_QksB8HUNiwSCYw08q4rapsJVw-wpPRFiBQgjhLlXYl-Z9WTQhyq6gFtN2XlH5jDcDwjfu4SL28blaDJjlYDVFlHP3A6rf_EWDfHZhls94d_nLBweM_Zv8jXcrJsrMuF-mAHM3G63HxBlqNNp8D9jgbJ_4Yc55uXGDLEGu1N8kGjJUXiD7Nh1OptEn4X=w1112-h625-no"
+				 alt="image001">
+			<h3 align="left"><b><u>Step 2:</u></b></h3>
+			<p align="left"><b>Select new option. And enter the new fiscal year details as shown below.</b></p>
+			<img src="https://lh3.googleusercontent.com/8ApQE5llx-Xgwt-ySXCXXiE40ndb7PPGDkxxSiJqM4ov6tRmKOaMCoRH6pZdUJ7I48-bIWAa_5K8AWuA7P-UHX8ydlC-kbT-GuZ_m-J7tIrYsRWnkB3TCgroFeioDLsMQawODOp2GP-J297UXQeLRDfdFUcBeISn_m0TMe1ZlOuxiNf49aUp7pTV1J4gGaHJzzjN-JGchDHBOgZpttlNBiphlnayOxhpItdWnIfFFKykZik2gQ00be2IvJe9EupBX9OeQv_uahEpcxIi1q_lJBfwUZr99P9VT3mNL0EEKySRuMATr0aQsdtSAZmtA_7VqZed30RZv_fZ-4oZCRzp4zVBEaVLneo1jvD8bgyfZo2CufWcb2TkqWMD0Vw8cFBpyQ2qRdYbHln8RDXSNnnxvyqwQ0jWFRmjTAPlghv4da5mUpWz97f1SRU4fTTC7uinUsNk50BhFLTBONcarFCEEiTzrH17XBnhKRQIqQ-wDKWwmhOaEgI76unhTsAG_UnkDKSFmx8L1Oaz_whWFwAZkEbb20q_ZSAi4mibVKHcYNudrkAYhW_9_UlhXJ2g0nvL9MqxkLC3-Jkgfys_h0XTW86eYcgxrhrbgk_ZS58vSs55r_1Huo1cyd11nQ8mcDK7gWBDcg1g6tp5HoYX_9MxiXs5hVcyYKB7VA7A_WOovOJqbVCG=w1112-h625-no"
+				 alt="image002">
+			<h3 align="left"><b><u>Step 3:</u></b></h3>
+			<p align="left"><b>Save and close the application.</b></p>
+			<h3 align="left"><b><u>Step 4:</u></b></h3>
+			<p align="left"><b>Reopen the software.</b></p>
+			<h3 align="left"><b><u>Step 5:</u></b></h3>
+			<p align="left"><b>Now while opening any window on software, a browse window with all fiscal year appears as
+					shown below. You can choose the required year.</b></p>
+			<img src="https://lh3.googleusercontent.com/696wJTtRmap8s1a-OviW7E8FxNssjBMmSSLvRh7lntN6kRlgxPcPVVQ2aUdcIYRbuCIPy-cIqwqSU_PX76kUZjxe4s8f0bLJP2kVCxBMIydQk61XKije2sCXNGVAcEpJ2vhW6ig86sY0ciDGpn3Xjza2UbN4JNIfHFAmIGRzrDoGIOO43DY2Lvk0mMlWiNZNITkLP2P8_8FVD2RlIKj5x6TO-j__m1nGTJne_GR_77KwqrbYL8kPJ46onpL3R8PyOJD13EosyyftU_EuRMgnEtv1MRpwwUwBZE_tFPUZMsFRMjdu5KiPK2InbEetQ0XPDd5p3HuiKMapXd1Da53oHGJOGqygAt7z3iZH-gRUmnHC_VVCH7T7Lk9Xy89T4Z7W_a2DFAbv8xpbdNvd3eBdP-lnTNfzDphbhvlwN6Mvg78Qmizsx3txU2KuyKDpiqnnYrnIS9i5eMSpvDLJTDtdg0NQmJpEe75XnFDll1TvPEffcNiXqrQqpRr5PfP7c1KitbX183YZFYOoVK79pTEwmtG6fmFZwiZkrQL6ro7rlyM1W2CeZllSJ5fwLsI9wj6sI2JWomuqu9Hw08HGOLMWHJJKg5LerLMNAz-ri3sQkViFKQFU6QezaqS6MXK48sxisLLhDYXIwvQ8xeZngt5lMZCIUduZZr7Mcn1Q1aIKd2kDYSf3=w1112-h625-no"
+				 alt="image003">
+
+
+		</div>
+	</div>@include("home.section_contact_wo_map")</div>
+	
+	<div id="loginModal" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content"><form method="post" action="{{ Route('login') }}" class="form-horizontal">{{ csrf_field() }}
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Login</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group clearfix">
+						<div class="col-xs-12">
+							<label class="control-label col-xs-4">Email Address:</label>
+							<div class="col col-xs-8">
+								<input type="text" name="email" value="" class="form-control">
+							</div>
+						</div>
+					</div>
+					<div class="form-group clearfix">
+						<div class="col-xs-12">
+							<label class="control-label col-xs-4">Password:</label>
+							<div class="col-xs-8">
+								<input type="password" name="password" value="" class="form-control">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+					<input type="submit" class="btn btn-primary" value="Login">
+				</div>
+			</form></div>
+		</div>
+	</div>
+@endsection
+@push('js')
+<script type="text/javascript">
+function login(){
+	$('#loginModal').modal('show');
+}
+</script>
+
+@endpush
