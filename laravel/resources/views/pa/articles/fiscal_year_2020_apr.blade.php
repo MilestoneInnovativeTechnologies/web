cc7c80e6ca13e5c92cb701f6b5906034132b29c7
Firose Hussain <github@firumon.com>
2020-04-03 21:26:28 +0530

    Images loaded from github

--- laravel/resources/views/pa/articles/fiscal_year_2020_apr.blade.php
+++ laravel/resources/views/pa/articles/fiscal_year_2020_apr.blade.php
@@ -22,11 +22,11 @@
 			<h2 align="left"> >> Open the ePlus software.</h2>
 			<h2 align="left"> >> In Tools menu select the option fiscal year.</h2>
 			<p align="left"><b>Go to Tools >Settings > Fiscal year</b></p>
-			<img src="https://lh3.googleusercontent.com/-RXBM86Imd9CRFmERCor4zRSA53UJE6S4pWoRwBKO1BQO-9hMn4iUunWn5xX5n3c-Vzh2w_bqnABytajFEYBDDKYWYsYgIFrH81391SJ6B9z1FkuezRG2Z6w0EKH43Gd-bmz3MxVPxmNA9WtYtU6h__6hSmEpo7XR9jI2C4br3FT1Z7d63tCFJfwa8Y8uSeH4cTjiBu-IKz9fcTjtYxrQR-HdwNTiNv9fa-rMK-SkWkNNxfsBxvw0_Hdjq3Z0p0TlSA_2wvWBa0TCaesxpz5E8DnUrkNylbk_SMUSG2pcCs9uOIVaZ94QPH4MWOTJPekhe9LgA_wpexKJzLGCioQkm_o0VQh5ROsfUCdKvV1KgfSvux24EDHUvJe47HwVHa0NWCO1-NZYRhyV4-CSY83LC4gWkEYcf2Bs7yEff1D9_qE40pxXOoP1-KRSfUCcz3B-rg8M73Q22LaUFrEZdLHUwrhdyEtmhCDnhiDUhXpDPtuCiZmJFviBIc73KnG7rKoOtEgtz0rQ3zwCXETuGj19p0M4su_T9__y0GP7LPxSmjTfdL0rpID5esJtzGwh1BuGyJyDcy4L_bOn6rbsNbIkXsKms3EWcufLTGa7_lLn4-gsu1_hwTxevYeUZQr06RxDuQB7rkyTHnMJxmxp3Tk4EaPHPCZJnEnGzIqbmmpeVG5ihs_ATz-QqGT9w9EQNJ_UG8=s1112-w1112-h625-no"
+			<img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/web/master/laravel/resources/views/pa/articles/files/IMG001.png"
 				 alt="image001">
 			<h3 align="left"><b><u>Step 2:</u></b></h3>
 			<p align="left"><b>Select new option. And enter the new fiscal year details as shown below.</b></p>
-			<img src="https://lh3.googleusercontent.com/r-yoX_Q2CklFMdd27HfZbQOmMuzToAZV5y5W8V2mHeza9KVUyIyaVXmrzWea5Sr3O8kuzH8-QeHc60w87Ho3tfIXLVe8u5lg2iT57cSVodCKeIWHAQnVT1L0b6aUTmlsW55b1_SHZkF4gfhhmLS0l6uZ77gSOqaH2LEH4_7U6_J5V7XyIwRABwFu9JoZOivQOCUdeWp_9sQHWMxEsQAxSVGM9bTpCR2jBQ0KXb2wY__WhWYKXLcw6lOp1TlkrpfgUd8j8VdKrezHzcdFNj8P0d2MWqOvxdadn21n1eif7KG-8v5OcIJzNgIACPCF2-Te9o0qtAfKNlXv9Mlb21eVfb20SaoKUQNSJwhTzTDo-jyMCscy2XRaRNaxfeuIDoYgFUtgFxsHNJrw4osbbjPs0asuLu5i9M14Wxcwx3RNo2L5S3Nh8T_X_GJTlzUzkXZBikHZ7XmDRNsytUNrfGhiep0vuKls4kC6Ss0m8s3IqgyVC3b4bBojJWUFrsLRmrhdW3o9W109cQ-3Us1dh3rDiXN4KMs79Ssv3P2MlP7-bo8KNG0lkkLW9vilL_4EoXlveHmYoh5ED_n6TMR7wwrqmkDM22soBIpGi1WLpHxKLL9LL-XtQQrtyrOOUDLz9bAiOi99RUEXFGeCOH05zgkUhc28aEBJ0UIEwdht_nws0F7jnfGyCX7_gT9YSNk=s1350-w1350-h540-no"
+			<img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/web/master/laravel/resources/views/pa/articles/files/IMG002.png"
 				 alt="image002">
 			<h3 align="left"><b><u>Step 3:</u></b></h3>
 			<p align="left"><b>Save and close the application.</b></p>
@@ -35,7 +35,7 @@
 			<h3 align="left"><b><u>Step 5:</u></b></h3>
 			<p align="left"><b>Now while opening any window on software, a browse window with all fiscal year appears as
 					shown below. You can choose the required year.</b></p>
-			<img src="https://lh3.googleusercontent.com/gyeP9T0rMBRPNHyPjYXQvW1H_Uwx_bW7sniEXFNZkslkvAKOoBcFOhgEtYYPndRc9ZJcB2h6IqIAQTpmLda8TB_L7eyP9aZRY1W4HklGn3WOUWYeW3G83ulbQpgQa2Kxjswtp6G917PHtg10Yhjksmn1OhO7HPJq53eruXtD4fEoIF9LXpJyD4QrZenpRalXFlJeg-C6lWVTbTHwn_xdmIA6Aftcfgmy1rXDV8ZBEU_xtYwxRsxpZN6wuW7uwGiL86pg-R1HCQaDtJDRmKTXKNr7BFEfH0Chl8zKhZ12bX9TMS09uEnIBJ5EMXeUIhFX1rGZ1aG-BWMvJKhjdPSGGz64piof2VK1B4zcYYHChkJzUE9bMjjL_rdUveLkhlXs2sLIEr_61rn9I4uQxuxaBTdOMykjHe_V-VIJVjQaRgUxk-65R4U-10PHo5QVtl3-mvkhg3bPdyaC9OFFM6OQDHsXTbvkT47S10WUPKiNMkz2LCrwUQbSfZZZYVaVrDJdQ10ZqclGTXMOELfhCPGaXDTbGgOqTMKDA5Ltlt6rGQFTSi0NVQV9d97hxNZmpnvRp7qWYThjvuIsR1ORmUr8elsc7Sff0QldTwJPrqMTVVChyM_NgRpJnLelUhPageolo4afmrQfUgzqX2DjbzAkMHDRgd1oHhj1HAHRGe_94Lal_853Mkv34rTAU00=s1343-w1343-h537-no"
+			<img src="https://raw.githubusercontent.com/MilestoneInnovativeTechnologies/web/master/laravel/resources/views/pa/articles/files/IMG003.png"
 				 alt="image003"><br><br><br>
 
 		</div>
